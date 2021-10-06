d058686c153a6bc64197b97be1d6617a5215f369
buivandieu <buivandieumdlshb@gmail.com>
2021-10-06 14:28:16 +0700

    SPL

---
+++ SQL/Assigment/BT1.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE Trainee;
+USE Trainee;
+
+CREATE TABLE Trainee (
+		TraineeID        INT,
+		Full_Name        VARCHAR(50),
+        Birth_Day        DATE,
+        Gender           VARCHAR(50),
+        ET_IQ            INT,
+        ET_Gmath         INT,
+        ET_English       INT,
+        Training_class   INT,
+        Evaluation_Notes VARCHAR(50)
+);
+        
